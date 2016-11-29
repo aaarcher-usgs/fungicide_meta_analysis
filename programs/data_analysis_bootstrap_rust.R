a2fa8671669c507677364bcb6e59822e1614a2f1
aaarchmiller <althea.archmiller@gmail.com>
2016-11-29 16:45:18 -0600

    bootstrapping for the categories

--- programs/data_analysis_bootstrap_rust.R
+++ programs/data_analysis_bootstrap_rust.R
@@ -28,20 +28,64 @@ remove(list=c(#"rust.data.ROM",
               "yield.data.ROM"
               ))
 
-mean.effectsizes <- NULL
-for(ii in 1:5000){
+#' Number of bootstrap simulations: 
+nsims <- 5
+
+#' Empty data frame to hold results
+results.rust <- as.data.frame(matrix(NA,ncol=14,nrow=nsims))
+colnames(results.rust) <- c("OVERALL",
+                            "DUAL","FLUT","MIXED","PYR","TEBU",
+                            "Strobilurin","Triaz_Strob","Triazole",
+                            "R1+", "R2+", "R3","R4","R5")
+
+#' Running the bootstraps
+for(ii in 1:nsims){
   studyIDS <- data.frame(ReferenceNumb = sample(rust.data.ROM$ReferenceNumb, 
                                                 length(unique(rust.data.ROM$ReferenceNumb)),
                                                 replace=T))
   newdata <- merge(studyIDS, rust.data.ROM)
+  # Overall analysis
   meta <- rma.uni(yi = yi,
                   vi = (n1i + n2i)/(n1i*n2i),
                   data = newdata,
                   method = "ML")
-  mean.effectsizes[ii] <- meta$b
+  results.rust$OVERALL[ii] <- meta$b
+  
+  # Active Ingredients
+  ai <- rma.uni(yi = yi,
+                vi = (n1i + n2i)/(n1i*n2i),
+                data = newdata,
+                method = "REML",
+                mods = ~category_ai-1)
+  results.rust[ii,2:6] <- ai$b
+  
+  # Classes
+  class <- rma.uni(yi = yi,
+                   vi = (n1i + n2i)/(n1i*n2i),
+                   data = newdata,
+                   method = "REML",
+                   mods = ~category_class-1)
+  results.rust[ii,7:9] <- class$b
+  
+  # Growth stage
+  rstage <- rma.uni(yi = yi,
+                    vi = (n1i + n2i)/(n1i*n2i),
+                    data = newdata,
+                    method = "REML",
+                    mods = ~category_rstage-1)
+  results.rust[ii,10:14] <- rstage$b
+  
 }
 
+class <- rma.uni(yi = yi,
+                 vi = (n1i + n2i)/(n1i*n2i),
+                 data = newdata,
+                 method = "REML",
+                 mods = ~category_rstage-1)
 
+#' Active ingredients
+
+test 
 
 #' ### Footer
 #' 
