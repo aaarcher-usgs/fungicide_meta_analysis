c531eb944ab88b08dec722507e39c8c51c90c29d
aaarchmiller <althea.archmiller@gmail.com>
2016-12-05 13:55:18 -0600

    main results plots; now have to go back to get year-categorical results for rust!

--- programs/results_plotting.R
+++ programs/results_plotting.R
@@ -52,6 +52,95 @@ ggplot(data = wide[!is.na(wide$Analysis.x),], aes(x = Mean, y = Mean.y))+
   geom_errorbar(aes(ymin=LL.y,ymax=UL.y))+
   geom_errorbarh(aes(xmin=LL,xmax=UL))
 
+#' ### Main results
+#' 
+pd <- position_dodge(width=0.2)
+#' Active Ingredient
+ggplot(data = summary.means[summary.means$Analysis=="Active Ingredient"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","FLUT","PYR","TEBU","MIXED","AZO + PROP"), 
+                   labels=c("Overall","FLUT","PYR","TEBU","Mixed","AZO + \nPROP"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Active Ingredient")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
+#' Fungicide Class
+ggplot(data = summary.means[summary.means$Analysis=="Fungicide Class"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","Strobilurin", "Triazole", "Triaz_Strob"), 
+                   labels=c("Overall","Strobilurin", "Triazole", "Mixed Triazole\n& Strobilurin"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Fungicide Class")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
+#' Disease Pressure
+summary.means$Analysis[summary.means$Category=="Seed Weight"&
+                         summary.means$Moderator=="medium"] <- NA
+ggplot(data = summary.means[summary.means$Analysis=="Disease Pressure"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","low","medium","high"), 
+                   labels=c("Overall","Low","Medium","High"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Disease Pressure")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
+#' Growth Stage
+ggplot(data = summary.means[summary.means$Analysis=="Growth Stage"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","R1+","R2+","R3","R5"), 
+                   labels=c("Overall","R1+","R2+","R3","R5"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Growth Stage")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
+#' Number of Applications
+ggplot(data = summary.means[summary.means$Analysis=="Applications"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","1 Application","2 Applications"), 
+                   labels=c("OVERALL","1 Application","2 Applications"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Applications")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
+#' Study Year
+ggplot(data = summary.means[summary.means$Analysis=="Study Year"|
+                              summary.means$Moderator=="OVERALL",], 
+       aes(x = Moderator, y = Mean, colour=Category, shape=Category))+
+  geom_pointrange(aes(ymin = LL, ymax = UL), position=pd)+
+  geom_hline(aes(yintercept=1), colour="grey")+
+  geom_vline(aes(xintercept=1.5), colour="grey")+
+  scale_x_discrete(limits=c("OVERALL","2006","2007","2008","2009","2010","2011","2012","2013"), 
+                   labels=c("OVERALL","2006","2007","2008","2009","2010","2011","2012","2013"))+
+  ylab("Mean Effect Size (95% C.I.)")+
+  xlab("Study Year")+
+  theme_tufte()+
+  theme(legend.position = "none")
+
 #' ### Footer
 #' 
 #' Spun with ezspin("programs/results_processing.R", out_dir="output", fig_dir="figures", keep_md=FALSE)
